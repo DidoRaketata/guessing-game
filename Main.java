1c61e830b3fc6b75fee0acc71558d7da1a90bf80
DidoRaketata <danielnikolov2009@gmail.com>
2025-11-26 15:13:30 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        
+        int b = rand.nextInt(11);
+        int attempts = 3;
+        boolean won = false;
+
+        for(int i = 0; i < attempts; i++){
+            if(i == 0) {
+                System.out.println("Guess a number between 0 and 10 (attempt 1)");
+            } else {
+                System.out.println("Try again (attempt: " + (i + 1) + ")");
+            }
+            
+            boolean validInput = false;
+            while(!validInput) {
+                if(sc.hasNextInt()){
+                    int a = sc.nextInt();
+                    validInput = true;
+                    
+                    if(a == b){
+                        System.out.println("You win!");
+                        won = true;
+                        break;
+                    }
+                } else {
+                    System.out.println("Invalid input. Please enter an integer between 0 and 10.");
+                    sc.next(); // consume the invalid input
+                }
+            }
+            
+            if(won) break;
+        }
+        
+        if(!won){
+            System.out.println("Game over! The number was: " + b);
+        }
+        sc.close();
+    }
+}
